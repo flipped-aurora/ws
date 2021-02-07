35e0f70da9ee2c2edd332d2001318992662ccca2
songzhibin97 <718428482@qq.com>
2021-02-07 16:26:14 +0800

    启动atomic.Value 换互斥锁

--- internal/data/data_manage.go
+++ internal/data/data_manage.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"fmt"
 	"sync"
-	"sync/atomic"
 )
 
 // Manage 管理所有客户端
@@ -18,7 +17,7 @@ type Manage struct {
 	// registry: 注册表
 	// map[string]biz.IClient
 	// map[key]biz.IClient
-	registry atomic.Value
+	registry map[string]biz.IClient
 
 	buf int64
 }
@@ -31,13 +30,7 @@ func (m *Manage) Register(key string) biz.IClient {
 	}
 	m.Lock()
 	defer m.Unlock()
-	oMap := m.registry.Load().(map[string]biz.IClient)
-	nMap := make(map[string]biz.IClient, len(oMap)+1)
-	for s, iClient := range oMap {
-		nMap[s] = iClient
-	}
-	nMap[key] = client
-	m.registry.Store(nMap)
+	m.registry[key] = client
 	fmt.Println("注册:", key)
 	return client
 }
@@ -48,27 +41,21 @@ func (m *Manage) UnRegister(key string) {
 		return
 	}
 	m.Lock()
-	oldMap := m.registry.Load().(map[string]biz.IClient)
-	nMap := make(map[string]biz.IClient, len(oldMap)-1)
-	var i biz.IClient
-	for s, iClient := range oldMap {
-		if s == key {
-			i = iClient
-			continue
+	if v, ok := m.registry[key]; ok {
+		delete(m.registry, key)
+		m.Unlock()
+		if v != nil && v != (biz.IClient)(nil) {
+			v.Shutdown()
 		}
-		nMap[s] = iClient
-	}
-	m.Unlock()
-	if i != nil && i != (biz.IClient)(nil) {
-		i.Shutdown()
 	}
 	fmt.Println("注销:", key)
 }
 
 // FindClient: 查找客户端
 func (m *Manage) FindClient(key string) (biz.IClient, bool) {
-	vMap := m.registry.Load().(map[string]biz.IClient)
-	v, ok := vMap[key]
+	m.Lock()
+	defer m.Unlock()
+	v, ok := m.registry[key]
 	return v, ok
 }
 
@@ -88,15 +75,16 @@ func NewManage(buf int64) *Manage {
 		buf: buf,
 		ctx: context.Background(),
 	}
-	m.registry.Store(make(map[string]biz.IClient))
+	m.registry = make(map[string]biz.IClient)
 	return m
 }
 
 // GetAll: 查找所有客户端
 func (m *Manage) GetAll() []biz.IClient {
-	vMap := m.registry.Load().(map[string]biz.IClient)
-	res := make([]biz.IClient, 0, len(vMap))
-	for _, client := range vMap {
+	m.Lock()
+	defer m.Unlock()
+	res := make([]biz.IClient, 0, len(m.registry))
+	for _, client := range m.registry {
 		res = append(res, client)
 	}
 	return res
